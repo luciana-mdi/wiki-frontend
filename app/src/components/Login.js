e7d5e706425af06208ca1e2c7c6b538009a60b73
luciana-mdi <luciana@mormedi.com>
2024-10-16 18:26:48 +0200

    updated login screen

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,17 +1,22 @@
 // src/components/Login.js
 
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import the useNavigate hook
 import './Login.css';
 import logo from '../assets/images/logo-mormedi-blanco.png'; // Import the logo image
-
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate(); // Initialize the useNavigate hook
 
   const handleLogin = (e) => {
     e.preventDefault();
     console.log('Login attempted', { username, password });
+
+    // Add your login logic here (e.g., validate user credentials)
+    // For demonstration purposes, let's assume the login is always successful.
+    navigate('/upload'); // Navigate to the upload screen on successful login
   };
 
   return (
